0fe4eecd891c4c9b05a9740b510a8e86eee93127
Coki <cokinew295@gmail.com>
2026-06-11 16:19:57 +0700

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Setting;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
 
                 View::share('setting', $setting);
 
+            }
+
+            if (app()->environment('production')) {
+                
+            URL::forceScheme('https');
+            
             }
 
         } catch (\Exception $e) {
